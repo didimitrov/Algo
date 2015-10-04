d9ed9a42885d685d7f0c0ecef6b90a7d5f9d3ad4
didimitrov <di_dimitrov_@abv.bg>
2015-10-04 11:26:59 +0300

    Update

---
+++ SortingAlgorithmsDemo/OOP/Exam/ConsoleForum/Console-Forum/Commands/MakeBestAnswerCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using ConsoleForum.Commands.Exceptions;
+using ConsoleForum.Contracts;
+using ConsoleForum.Entities.Posts;
+
+namespace ConsoleForum.Commands
+{
+    class MakeBestAnswerCommand:AbstractCommand
+    {
+        public MakeBestAnswerCommand(IForum forum) : base(forum)
+        {
+        }
+
+        public override void Execute()
+        {
+            int answerId = int.Parse(this.Data[1]);
+            var question = this.Forum.CurrentQuestion;
+
+            if (!this.Forum.IsLogged)
+            {
+                throw new CommandException(Messages.NotLogged);
+            }
+            if (this.Forum.CurrentQuestion == null)
+            {
+                throw new CommandException(Messages.NoQuestionOpened);
+            }
+            if (!question.Answers.Any(a => a.Id == answerId))
+            {
+                throw new CommandException(Messages.NoAnswer);
+            }
+            if (question.Author != this.Forum.CurrentUser && !(this.Forum.CurrentUser is IAdministrator))
+            {
+                throw new CommandException(Messages.NoPermission);
+            }
+            if (question.Answers.Any(a => a is BestAnswer))
+            {
+                int currentBestID = question.Answers.Where(a => a is BestAnswer).FirstOrDefault().Id;
+                Answer oldBest = new Answer(currentBestID, question.Answers[currentBestID - 1].Body, question.Answers[currentBestID - 1].Author);
+                question.Answers[currentBestID - 1] = oldBest;
+            }
+            IAnswer answer = question.Answers.FirstOrDefault(a => a.Id == answerId);
+            BestAnswer bestAnswer = new BestAnswer(answer.Id, answer.Body, answer.Author);
+            question.Answers[answerId - 1] = bestAnswer;
+            this.Forum.Output.AppendLine(string.Format(Messages.BestAnswerSuccess, answerId));
+        }
+    }
+}
---
+++ SortingAlgorithmsDemo/OOP/Exam/ConsoleForum/Console-Forum/Commands/PostAnswerCommand.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleForum.Commands.Exceptions;
+using ConsoleForum.Contracts;
+using ConsoleForum.Entities.Posts;
+
+namespace ConsoleForum.Commands
+{
+    class PostAnswerCommand:AbstractCommand
+    {
+        public PostAnswerCommand(IForum forum) : base(forum)
+        {
+        }
+
+        public override void Execute()
+        {
+            var body = Data[1];
+            var question = this.Forum.CurrentQuestion;
+
+            if (!Forum.IsLogged)
+            {
+                throw new CommandException(Messages.NotLogged);
+            }
+            if (question==null)
+            {
+                throw new CommandException(Messages.NoQuestion);
+            }
+
+            var answer = new Answer((Forum.Answers.Count + 1), body, Forum.CurrentUser);
+            Forum.Answers.Add(answer);
+            question.Answers.Add(answer);
+            this.Forum.Output.AppendLine(string.Format(Messages.PostAnswerSuccess, answer.Id));
+        }
+    }
+}
