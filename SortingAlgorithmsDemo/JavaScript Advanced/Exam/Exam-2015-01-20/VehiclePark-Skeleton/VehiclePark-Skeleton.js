2fdf2594c820522f79e7b5a834d06bbfa5c4fb22
didimitrov <di_dimitrov_@abv.bg>
2015-10-21 19:55:12 +0300

    Update

--- SortingAlgorithmsDemo/JavaScript Advanced/Exam/Exam-2015-01-20/VehiclePark-Skeleton/VehiclePark-Skeleton.js
+++ SortingAlgorithmsDemo/JavaScript Advanced/Exam/Exam-2015-01-20/VehiclePark-Skeleton/VehiclePark-Skeleton.js
@@ -142,26 +142,130 @@ function processVehicleParkCommands(commands) {
                 return Vehicle.prototype.getVehicleInfo.call(this) +
                     ",frameSize=" + this.getFrame() +
                     (this.getShifts() ? ",numberOfShifts=" + this.getShifts() : "");
-            }
+            };
 
             return Bike;
         })();
 
 
-        var Automobile = {
-            // TODO: Implement this class
-        }
+        var Automobile = (function () {
+            Automobile.extend(Vehicle);
 
-        var Truck = {
-            // TODO: Implement this class
-        }
+            function Automobile(brand, age, terrainCoverage, numberOfWheels, consumption, typeOfFuel ){
+                if (this.constructor === Vehicle) {
+                    throw new Error("Cannot create instance of abstract class Vehicle.")
+                }
+                Vehicle.apply(this, arguments);
+                this.setConsumption(consumption);
+                this.setTypeOfFuel(typeOfFuel);
+            }
 
-        var Limo = {
-            // TODO: Implement this class
-        }
+            Automobile.prototype.getConsumption = function () {
+                return this._consumption;
+            };
+            Automobile.prototype.setConsumption = function (consumption) {
+                if (consumption === undefined || isNaN(consumption) || consumption < 0) {
+                    throw new Error("consumption cannot be negative.");
+                }
+                return this._consumption = consumption;
+            };
+
+            Automobile.prototype.getTypeOfFuel = function () {
+                return this._typeOfFuel;
+            };
+            Automobile.prototype.setTypeOfFuel = function (typeOfFuel) {
+                if (typeOfFuel === undefined ) {
+                    throw new Error("typeOfFuel cannot be empty.");
+                }
+                return this._typeOfFuel= typeOfFuel;
+            };
+
+            Automobile.prototype.toString = function () {
+                return Vehicle.prototype.toString.call(this)+
+                    ",consumption=[" + this.getConsumption() + "l/100km " + this.getTypeOfFuel() + "]";
+            };
+            
+            return Automobile;
+        })();
+
+        var Truck = (function () {
+            var TRUCK_NUMBEROFWHEELS = 2;
+            var TRUCK_TERRAIN_COVERAGE = 'all';
+
+            Truck.extend(Automobile);
+
+            function Truck(brand, age, consumption, typeOfFuel,numberOfDoors){
+                Automobile.call(this, brand, age, TRUCK_NUMBEROFWHEELS, consumption, numberOfDoors, TRUCK_TERRAIN_COVERAGE,typeOfFuel)
+                this.setDoors(numberOfDoors);
+            }
+
+            Truck.prototype.getDoors = function () {
+                return this._doors;
+            };
+
+
+            Truck.prototype.setDoors = function (numberOfDoors) {
+                if (numberOfDoors === undefined || isNaN(numberOfDoors) || numberOfDoors < 0) {
+                    throw new Error("numberOfDoors cannot be negative.");
+                }
+                return this._doors = numberOfDoors;
+            };
+
+
+            Truck.prototype.toString = function () {
+                return Automobile.prototype.toString.call(this)+ ",numberOfDoors=" + this.getDoors();
+            };
+            return Truck;
+        })();
+
+        var Limo = (function () {
+            var LIMO_TERRAINCOVERAGE = "road";
+
+            Limo.extend(Automobile);
+
+            function Limo(brand, age, numberOfWheels, consumption, typeOfFuel){
+                Automobile.call(this, brand, age, LIMO_TERRAINCOVERAGE, numberOfWheels, consumption, typeOfFuel)
+                this._emloyees = [];
+            }
+
+            Limo.prototype.getEmployees = function () {
+                return this._emloyees;
+            };
+            Limo.prototype.appendEmployee = function (employee) {
+                var index = this.getEmployees().indexOf(employee);
+                if(index == -1){
+                    this.getEmployees().push(employee)
+                }
+            };
+            Limo.prototype.detachEmployee = function (employee) {
+                var index = this.getEmployees().indexOf(employee);
+                if(index !== -1){
+                    this.getEmployees().remove(employee);
+                }
+                else {
+                throw new Error("Limo does not contain such employee.");
+            }
+            };
+
+            Limo.prototype.getEmployeesInfo = function () {
+                var employeesInfo = " --> Employees, allowed to drive:" +
+                    (this.getEmployees().length > 0 ? "\n" + this.getEmployees().join("\n") : " ---");
+
+                return employeesInfo;
+            }
+
+            Limo.prototype.toString = function () {
+                return Automobile.prototype.toString.call(this) + "\n" + this.getEmployeesInfo();
+            };
+
+            return Limo;
+        })();
 
         return {
-            Employee: Employee
+            Employee: Employee,
+            Bike: Bike,
+            Truck: Truck,
+            Limo: Limo
         }
     }());
 
@@ -260,6 +364,29 @@ function processVehicleParkCommands(commands) {
                     limos[i].appendEmployee(employee);
                 }
                 return "Added employee to possible Limos.";
+            }
+
+            function processListEmployees(command){
+                var grade = command["grade"];
+                var filteredEmployees;
+                var sortedEmployees;
+
+                if(grade== 'all'){
+                    filteredEmployees= _employees;
+                }
+                else{
+                    grade= parseFloat(grade);
+                    filteredEmployees = _employees.filter(function (e) {
+                        return grade <= e.getGrade();
+                    });
+                }
+
+                sortedEmployees = filteredEmployees
+                    .sort(function(e1, e2) {
+                        return e1.getName().localeCompare(e2.getName());
+                    });
+
+                return formatOutputList(sortedEmployees);
             }
 
             function processDetachEmployeeCommand(command) {
