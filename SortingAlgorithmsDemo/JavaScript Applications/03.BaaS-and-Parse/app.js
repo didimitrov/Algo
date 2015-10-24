d73238f8860895736a6c95c33c844fab0dd71d84
didimitrov <di_dimitrov_@abv.bg>
2015-10-24 09:24:13 +0300

    Update

---
+++ SortingAlgorithmsDemo/JavaScript Applications/03.BaaS-and-Parse/app.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    var COUNTRIES_LIST_ID = "zglO59FNb78GVcUsUOJLp1tqfLxcZ3u3C8a88hox";
+    var COUNTRIES_LIST_KEY = "vgv5unJ0bLI5N1fs0NfgOrD2GnSGCEjzdPCNbDZO";
+    var headers = {
+        "X-Parse-Application-Id": COUNTRIES_LIST_ID,
+        "X-Parse-REST-API-Key": COUNTRIES_LIST_KEY
+    };
+    loadCountries();
+
+    function townsRequest() {
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+
+        $.ajax({
+            method: 'get',
+            headers: headers,
+            url: 'https://api.parse.com/1/classes/Town?where='+
+            '{"country": {"__type": "Pointer","className": "Country", "objectId": "' + $(this).attr('objectId') +'"}}'
+        }).success(function (data) {
+            $('#towns').children().remove();
+            for (var i in data.results) {
+                var town = data.results[i];
+                var townItem = $('<li>').text(town.name);
+                townItem.appendTo($('#towns'));
+            }
+        }).error(function () {
+            alert('Towns cant be loaded')
+        });
+    }
+
+   function loadCountries(){
+       $('#countries').children().hide();
+       $.ajax({
+           method: 'get',
+           headers: headers,
+           url: "https://api.parse.com/1/classes/Country"
+       }).success(function(data){
+           for (var i in data.results) {
+               var country = data.results[i];
+               var countryItem = $('<li>').text(country.name);
+               $(countryItem).attr('objectId', country.objectId);
+               countryItem.appendTo($('#countries'));
+               countryItem.on('click',townsRequest)
+           }
+       }).error(function () {
+           alert('Cannot load countries')
+       });
+   }
+
+    $.ajax({
+        method: 'get',
+        headers: headers,
+        url: "https://api.parse.com/1/classes/Town?order=name"
+    }).success(function (data){
+        for (var i in data.results) {
+            var town = data.results[i];
+            var townItem = $('<li>').text(town.name);
+            townItem.appendTo($('#towns'));
+        }
+    }).error(function () {
+        alert('Towns cant be loaded')
+    });
+
+   var addCountry = function () {
+       var input = $('#newCountryName').val();
+       if (input) {
+           $.ajax({
+               method: 'post',
+               headers: headers,
+               url: "https://api.parse.com/1/classes/Country",
+               contentType: 'application/json',
+               data: JSON.stringify({"name": input})
+           }).success(function () {
+               loadCountries();
+              // alert("New country successfully added");
+               input.remove();
+           }).error(function () {
+               alert('Error occurred while adding new country')
+           });
+       }
+   };
+    $('a').on('click', addCountry);
+
+});
